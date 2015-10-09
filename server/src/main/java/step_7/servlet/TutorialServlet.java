ae30ac00e988c10d9ec76d1bed07a61e2aea8d2d
gema <gema.santiago@canoo.com>
2015-10-09 11:17:09 +0200

    updated open-dolphin to version 1.0-RC1

--- server/src/main/java/step_7/servlet/TutorialServlet.java
+++ server/src/main/java/step_7/servlet/TutorialServlet.java
@@ -1,12 +1,12 @@
 package step_7.servlet;
 
-import org.opendolphin.core.server.ServerDolphin;
+import org.opendolphin.core.server.DefaultServerDolphin;
 import org.opendolphin.server.adapter.DolphinServlet;
 import step_7.TutorialDirector;
 
 public class TutorialServlet extends DolphinServlet{
     @Override
-    protected void registerApplicationActions(ServerDolphin serverDolphin) {
+    protected void registerApplicationActions(DefaultServerDolphin serverDolphin) {
         serverDolphin.register(new TutorialDirector());
     }
 }
